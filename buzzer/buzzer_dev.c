4151fd85f9db86f41d7709310dd99e379d020fa8
wooseok <dntjr4772@naver.com>
2019-12-01 12:13:10 +0000

    buzzer

--- buzzer/buzzer_dev.c
+++ buzzer/buzzer_dev.c
@@ -11,116 +11,72 @@
 #include <asm/uaccess.h>
 
 #define BUZZER_MAJOR_NUMBER 505
-#define BUZZER_DEV_NAME   "buzzer_pwm"
-
+#define BUZZER_DEV_NAME   "buzzer"
+//gpio25
 #define GPIO_BASE_ADDR 0x3F200000
-#define GPFSEL1 0X04
-#define GPFSEL4 0X10
-
-//PWM
-#define PWM_BASE_ADDR 0x3F20C000
-//
-#define PWM_CTL 0x00
-#define PWM_RNG1 0x10
-#define PWM_DAT1 0x14
-
-// clock control
-#define CLK_BASE_ADDR 0x3F101000
-//offset
-#define CLK_PWM_CTL 0xa0
-#define CLK_PWM_DIV 0xa4
-
-//PASSWORD
-#define BCM_PASSWORD 0x5A000000
+#define GPFSEL2 0X08
+#define GPSET0   0x1C
+#define GPCLR0   0x28
 
 static void __iomem *gpio_base;
-volatile unsigned int *gpsel1;
-volatile unsigned int *gpsel4;
-
-static void __iomem *pwm;
-volatile unsigned int *pwmctl;
-volatile unsigned int *pwmrng1;
-volatile unsigned int *pwmdat1;
-
-static void __iomem *clk;
-volatile unsigned int *clkdiv;
-volatile unsigned int *clkctl;
+volatile unsigned int *gpsel2;
+volatile unsigned int * gpset0; 
+volatile unsigned int * gpclr0;
 
 #define IOCTL_MAGIC_NUMBER 'b'
 #define IOCTL_CMD_SET_DIRECTION _IOWR(IOCTL_MAGIC_NUMBER, 0, int)
 #define IOCTL_CMD_CLEAR_DIRECTION _IOWR(IOCTL_MAGIC_NUMBER,1,int)
-int init_pwm(void) {
-   int pwm_ctrl = *pwmctl;
-   *pwmctl = 0; 					// store PWM control and stop PWM
-   msleep(10);						// sleep
-   *clkctl = BCM_PASSWORD | (0x01 << 5); // stop PWM Clock
-   msleep(10);						//sleep
-   
-   int idiv = (int)(330); // Oscilloscope to 16kHz
-   *clkdiv = BCM_PASSWORD | (idiv << 12); // integer part of divisior register
-   *clkctl = BCM_PASSWORD | (0x11); //set source to oscilloscope & enable PWM CLK
-
-   *pwmctl = pwm_ctrl; 				// restore PWM control and enable PWM
-}
 
 int buzzer_open(struct inode *inode, struct file *filp){
    printk(KERN_ALERT "buzzer driver open!!\n");
    
    gpio_base = ioremap(GPIO_BASE_ADDR, 0x60);
-   gpsel1=(volatile unsigned int *)(gpio_base+GPFSEL1);
-   gpsel4=(volatile unsigned int *)(gpio_base+GPFSEL4);
-
-   *gpsel4&=(0<<2);
-   *gpsel4|=(1<<2);
-   *gpsel4|=(1);
-   
-
-   clk = ioremap(CLK_BASE_ADDR, 0xFF);   //size
-   clkctl=(volatile unsigned int*)(clk+CLK_PWM_CTL);
-   clkdiv=(volatile unsigned int*)(clk+CLK_PWM_DIV);
-   
-   pwm = ioremap(PWM_BASE_ADDR, 0xFF);
-   pwmctl = (volatile unsigned int*)(pwm+PWM_CTL);
-   pwmrng1 = (volatile unsigned int*)(pwm+PWM_RNG1);
-   pwmdat1 = (volatile unsigned int*)(pwm+PWM_DAT1);
-	
+   gpsel2=(volatile unsigned int *)(gpio_base+GPFSEL2);
+   gpset0 = (volatile unsigned int *)(gpio_base + GPSET0);
+   gpclr0 = (volatile unsigned int *)(gpio_base + GPCLR0);
+   *gpsel2&=(0<<17);
+   *gpsel2&=(0<<16);
+   *gpsel2|=(1<<15);
    return 0;
 }
 
 int buzzer_release(struct inode *inode, struct file *filp){
    printk(KERN_ALERT "buzzer driver close!!\n");
    iounmap((void*)gpio_base);
-   iounmap((void*)clk);
-   iounmap((void*)pwm);
    return 0;
 }
+
 long buzzer_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-   int kbuf = 0;
-   int i = 0;
-   init_pwm();
+   int situation = 0;
+   int a=0,i = 0;
+   copy_from_user(&situation,(const void*)arg,4);
    switch(cmd) {
-      //Not only direction setting but PWM Setting. 
+      
       case IOCTL_CMD_SET_DIRECTION:
-         //Original Code of motor_IOCTL
-         /////copy_from_user(&kbuf, (const void*)arg, 4);
-      //   *gpsel1 &= ~(1<<26);   //Alternate function5!!!
-       //  *gpsel1 |= (1<<25);
-       //  *gpsel1 &= ~(1<<24);
          printk(KERN_ALERT "buzzer set direction out!!\n");
-         *pwmctl |= (1);      		//PWEN       1
-         *pwmctl &= ~(1<<1);    	//MODE1      0
-         *pwmctl |= (1<<7);    		//MSEN1      1
-         *pwmrng1 = 320;      		//RANGE      320
-         *pwmdat1|= (1<<5);   	 	//DAT     	 32   (1/10)
-         printk(KERN_ALERT,"%x",*pwmdat1);
-         break;
-      case IOCTL_CMD_CLEAR_DIRECTION: 
-         printk(KERN_ALERT "buzzer clear direction out!!\n");
-         *pwmdat1&= ~(1<<5);
-         *pwmdat1|= (1<<4);   	 	
-         *pwmdat1|= (1<<3);         //DAT     	 24   (1.5/20)
-         printk(KERN_ALERT,"%x",*pwmdat1);
+         if(situation==0){
+            for(a=0;a<5;a++){
+               for(i=0;i<1000;i++){
+                  *gpset0 |= (0x01 << 25); 
+                  udelay(100);
+                  *gpclr0 |= (0x01 << 25); 
+                  udelay(500);
+               }
+               mdelay(10);
+            }
+         }else if(situation==1){
+            for(a=0;a<5;a++){
+               for(i=0;i<200;i++){
+                  *gpset0 |= (0x01 << 25); 
+                  udelay(1000);
+                  *gpclr0 |= (0x01 << 25); 
+                  udelay(500);
+               }
+               mdelay(10);
+            }
+         }
+         
          break;
    }
 
