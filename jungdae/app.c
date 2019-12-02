dce5e54a2866529012f9d5c28e46bca1292e88c9
jmingrove21 <jmingrove21@naver.com>
2019-12-02 14:14:38 +0000

    jmk commit

--- jungdae/app.c
+++ jungdae/app.c
@@ -129,7 +129,7 @@ int main(int argc, char** argv)
             readBytes = read(connectFD, receiveBuffer, BUFF_SIZE);
            
             int recv_value=atoi(receiveBuffer);
-            printf("send : %d recv : %d \n",ultra_status,recv_value);
+            printf("send : %d recv : %d \n",send_flag,recv_value);
             
             if(recv_value!=0){
                 flag++;
